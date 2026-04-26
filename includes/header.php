ecd8605ab7746b1a7328b687fafdf203022b7a4b
mahmoud diaa <mahmouddiaa2021@gmail.com>
2026-04-27 01:58:55 +0300

    nav

--- includes/header.php
+++ includes/header.php
@@ -8,14 +8,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Smart Insurance</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/nav.css">
 </head>
 <body>
 <header>
-    <nav>
-        <a href="../index.php">Home</a>
-        <a href="../auth/login.php">Login</a>
-        <a href="../auth/register.php">Register</a>
-    </nav>
+    <div class="container">
+        <img class="logo" src="images/DONE.jfif" alt="COVERLY">
+
+        <nav>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                
+                <li class="dropdown">
+                    <a href="#" class="dropbtn"> 
+                        Categories <i class="fas fa-chevron-down"></i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Health</a></li>
+                        <li><a href="#">Car</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                
+                <li class="profile">
+                    <a href="profile.html">
+                        <i class="fa-regular fa-circle-user"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
 <main>
