f2709c5bc5a8f8dc2076b5308f70fe5a980da292
HZOOMAAA <Hazem.Mohamed.22230228@commerce.helwan.edu.eg>
2026-04-28 17:37:10 +0300

    fixing the nav and footer path

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Smart Insurance</title>
-    <link rel="stylesheet" href="../assets/css/nav.css">
+    <link rel="stylesheet" href="assets/css/nav.css">
 </head>
 <body>
 <header>
